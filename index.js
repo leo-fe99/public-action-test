5ff1a1421ba5dc1dadb011eb10b86863db9f72ad
leo-fe99 <leonardoferro957@gmail.com>
2022-09-22 13:46:32 +0200

    Test v1.6

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const fs = require("fs");
 
 try {
   const nameToGreet = core.getInput("who-to-greet");
+  const jsonResult = core.getInput("jsonResult");
   const time = new Date().toTimeString();
   const jsonDataPath = "test.json";
   const jsonData = fs.readFileSync(jsonDataPath).toString();
   console.log(`Hello ${nameToGreet}!`);
   core.setOutput("time", time);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(jsonData);
+  console.log(jsonResult);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
