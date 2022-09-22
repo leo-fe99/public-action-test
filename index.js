1d6c33b7fa15a9772fbe445082495f05779d0c3b
leo-fe99 <leonardoferro957@gmail.com>
2022-09-22 13:34:33 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
   const time = new Date().toTimeString();
-
+  const jsonDataPath = "test.json";
+  const jsonData = fs.readFileSync(jsonDataPath).toString();
   console.log(`Hello ${nameToGreet}!`);
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(jsonData);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
