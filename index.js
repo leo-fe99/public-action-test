c343c6359cad81ea3b4a3b44e685a7d7398cb5d2
Leonardo Ferro <leonardoferro957@gmail.com>
2022-11-08 15:44:29 +0100

    Test

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
-const fs = require("fs");
 
 try {
-  const nameToGreet = core.getInput("who-to-greet");
-  const jsonResult = core.getInput("jsonResult");
-  const time = new Date().toTimeString();
-  const jsonDataPath = "test.json";
-  const jsonData = fs.readFileSync(jsonDataPath).toString();
-  console.log(`Hello ${nameToGreet}!`);
-  core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(jsonResult);
-  console.log(`The event payload: ${payload}`);
+  const variable = core.getInput("variable");
+  console.log(variable)
 } catch (error) {
   core.setFailed(error.message);
 }
