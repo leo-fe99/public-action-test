ca2f8c8e9022ad11adafc8c7a9636d1eb461fd0c
Leonardo Ferro <leonardoferro957@gmail.com>
2022-11-17 14:24:17 +0100

    Parsing json file

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const core = require("@actions/core");
 try {
   const variable = core.getInput("variable");
   console.log(variable)
+  console.log(typeof variable)
+  try{
+    console.log(variable[0])
+  }
+  catch(e){
+    console.log("error")
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
